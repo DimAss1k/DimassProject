6f82222a25fd319ccc1ed7cac368262f00f64e56
Dim <dmukhin@sfedu.ru>
2025-04-14 12:36:34 +0300

    bd

---
+++ piris.DomainService/IDatabaseService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace piris.DomainService.DbContext
+{
+    [ServiceContract]
+    public interface IDatabaseService
+    {
+        [OperationContract]
+        List<store_positions> GetPositions();
+
+        [OperationContract]
+        bool AddPosition(store_positions position);
+
+        [OperationContract]
+        store_positions GetPositionById(int id);
+
+        [OperationContract]
+        store_users GetUserByUserName(string userName);
+
+        [OperationContract]
+        store_positions UpdatePosition(store_positions
+position);
+
+        [OperationContract]
+        bool DeletePosition(int id);
+
+        [OperationContract]
+        bool CreateUser(store_users user);
+    }
+}
